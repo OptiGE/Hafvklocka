f5e3fc305b0798c17ad62a2e9ef0f90eefa969ba
OptiGE <optige@gmail.com>
2018-10-27 11:43:59 +0200

    Ändrade ikon till Delta, fått timern att funka

--- kod/files/hafvklocka.js
+++ kod/files/hafvklocka.js
@@ -1,7 +1,75 @@
+
+//------------------------------------------------------------------------------------
+//------------------------- T I M E R F U N K T I O N E R-----------------------------
+//------------------------------------------------------------------------------------
+
+var time_cs = 0;
+var time_s = 0;
+var time_m = 0;
+
+var timer_active = false;
+var cooldown_time = 100;
+
+function resetTimer() {
+    time_cs = 0;
+    time_s = 0;
+    time_m = 0;
+    timer_active = false;
+    document.getElementById("timer_text").innerHTML = "00:00:00";
+}
+
+function toggleTimer() {
+    
+    timer_active = !timer_active;
+    if(document.getElementById("togglebtn").innerHTML == "Start"){
+        document.getElementById("togglebtn").innerHTML = "Pause";
+    }else{
+        document.getElementById("togglebtn").innerHTML = "Start";
+    }
+}
+
+function timerLoop() {
+    
+    //Om timern är igång
+    if (timer_active){
+            time_cs ++;
+    
+        //Centisekunder till sekunder
+        if(time_cs == 100){
+            time_cs = 0;
+            time_s ++;
+        }
+
+        //Sekunder till minuter
+        if(time_s == 60){
+            time_s = 0;
+            time_m ++;
+        }
+
+        document.getElementById("timer_text").innerHTML = time_m + ":" + time_s + ":" + time_cs;
+    }
+    
+}
+
+// Will execute timerLoop every 0.01 seconds 
+var intervalID = window.setInterval(timerLoop, 10);
+
+//------------------------------------------------------------------------------------
+//--------------------------- A C C F U N K T I O N E R-------------------------------
+//------------------------------------------------------------------------------------
+
 var axmax = 0;
 var aymax = 0;
 var azmax = 0;
 
+var slider = document.getElementById("force_slider");
+var output = document.getElementById("demo");
+output.innerHTML =  "Styrka: " + slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+output.innerHTML = "Styrka: " + this.value;
+}
 
 //Om enheten har ett motionEvent (alltså om det har ett gyroskop eller accelerometer)
 if (window.DeviceMotionEvent != undefined) {
@@ -10,10 +78,14 @@ if (window.DeviceMotionEvent != undefined) {
 	//När en ny rörelse upptäcks
 	window.ondevicemotion = function(e) {
 	
-		console.log("Rörelse detekterad");
-		console.log(e.acceleration.x);
-		
+        //Om rörelsen är större än värdet på slidern.
+        if(e.acceleration.z > slider.value){
+			toggleTimer();
+		}
+        
+        
 		//Om den nya rörelsen va större än någon innan
+        /*
 		if(e.acceleration.x > axmax){
 			axmax = e.acceleration.x;
 			document.getElementById("outputx").innerHTML = "Max x: " + axmax;
@@ -26,15 +98,9 @@ if (window.DeviceMotionEvent != undefined) {
 			azmax = e.acceleration.z;
 			document.getElementById("outputz").innerHTML = "Max z: " + azmax;
 		}
+        */
 		
 	}
-}else{console.log("Enheten saknar accelerometer");}
-
-var slider = document.getElementById("force_slider");
-var output = document.getElementById("demo");
-output.innerHTML =  "Styrka: " + slider.value; // Display the default slider value
-
-// Update the current slider value (each time you drag the slider handle)
-slider.oninput = function() {
-output.innerHTML = "Styrka: " + this.value;
+}else{
+    console.log("Enheten saknar accelerometer");
 }
